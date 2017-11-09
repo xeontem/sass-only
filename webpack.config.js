19c0dee98037125b1171408ab88fc2d9d3b66312
xeontem <xeontem@gmail.com>
2017-11-09 20:17:36 +0300

    make a boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/styles/main.scss',
+  },
+  output: {
+    path: path.join(__dirname, './dist/'),
+    filename: '[name].css' // output js file name is identical to css file name
+  },
+  devServer: {
+      contentBase: path.resolve(__dirname, 'dist'),
+      hot: true,
+      port: 9000
+    },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // use style-loader in development
+                fallback: "style-loader"
+            })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}  
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+        title: 'sass-only',
+        favicon: './src/images/favicon.png',
+        template: path.resolve(__dirname, 'src/index.html'),
+      }),
+    new ExtractTextPlugin('[name].css') // css file will override generated js file
+  ]
+}
